0cece2a4f2735b9f26a99eb174922be09726c249
zhz8888 <67500896+zhz8888@users.noreply.github.com>
2020-08-29 13:04:44 +0800

    Update and rename OmvInPve.sh to OpenMediaVault-in-Proxmox_VE-CN.sh

--- OpenMediaVault-in-Proxmox_VE-CN.sh
+++ OpenMediaVault-in-Proxmox_VE-CN.sh
@@ -17,72 +17,11 @@ ver=`/usr/bin/pveversion|awk -F'/' 'NR==1{print $2}'|awk -F'-' '{print $1}'`
 pve=$OS$ver
 if [ "$OS" != "pve" ];then
     echo "您的系统不是 Proxmox VE , 无法安装!"
-    echo "Your OS is not Proxmox VE that cannot install!"
     exit
 fi
 
-pve53(){
-    apt-mark unhold openmediavault
-    apt-get autoremove openmediavault
-    apt-get -f -y install apparmor attr bridge-utils ceph-common ceph-fuse cifs-utils corosync criu cstream dtach ebtables \
-faketime file fonts-font-awesome genisoimage glusterfs-client glusterfs-common ipset libacl1-dev \
-libanyevent-http-perl libanyevent-perl libapparmor-perl libappconfig-perl libapt-pkg-perl libarchive13 \
-libasound2 libasound2-data libattr1-dev libauthen-pam-perl libbabeltrace-ctf1 libbabeltrace1 \
-libboost-iostreams1.62.0 libboost-program-options1.62.0 libboost-random1.62.0 libboost-regex1.62.0 \
-libboost-system1.62.0 libboost-thread1.62.0 libbytes-random-secure-perl libc-dev-bin libc6-dev \
-libcaca0 libcephfs1 libcfg6 libclone-perl libcmap4 libcommon-sense-perl libconvert-asn1-perl \
-libcorosync-common4 libcpg4 libcrypt-openssl-bignum-perl libcrypt-openssl-random-perl \
-libcrypt-openssl-rsa-perl libcrypt-random-seed-perl libcrypt-ssleay-perl libdevel-cycle-perl \
-libdigest-hmac-perl libdw1 libencode-locale-perl libfaketime libfcgi0ldbl libfdt1 libfile-chdir-perl \
-libfile-listing-perl libfile-readbackwards-perl libfilesys-df-perl libgnutlsxx28 libgoogle-perftools4 \
-libhtml-parser-perl libhtml-tagset-perl libhtml-tree-perl libhttp-cookies-perl libhttp-daemon-perl \
-libhttp-date-perl libhttp-message-perl libhttp-negotiate-perl libibverbs1 libio-html-perl \
-libio-multiplex-perl libio-socket-inet6-perl libio-socket-ssl-perl libio-stringy-perl libipset3 \
-libiscsi7 libjemalloc1 libjs-extjs libjson-c3 libjson-xs-perl liblinux-inotify2-perl \
-liblwp-mediatypes-perl liblwp-protocol-https-perl libmath-random-isaac-perl libmime-base32-perl \
-libnet-dbus-perl libnet-dns-perl libnet-http-perl libnet-ip-perl libnet-ldap-perl libnet-ssleay-perl \
-libnet1 libnetfilter-log1 libnl-route-3-200 libnspr4 libnss3 libnuma1 libprotobuf-c1 libprotobuf10 \
-libpve-access-control libpve-apiclient-perl libpve-common-perl libpve-guest-common-perl \
-libpve-http-server-perl libpve-storage-perl libqb0 libquorum5 librados2 \
-librados2-perl libradosstriper1 librbd1 librdmacm1 librgw2 librrds-perl libsdl1.2debian libsmbclient \
-libsnappy1v5 libsocket6-perl libstatgrab10 libstring-shellquote-perl libtcmalloc-minimal4 \
-libtemplate-perl libterm-readline-gnu-perl libtimedate-perl libtotem-pg5 libtypes-serialiser-perl \
-libu2f-server0 libunwind8 liburcu4 liburi-perl libusbredirparser1 libuuid-perl libvotequorum8 \
-libwww-perl libwww-robotrules-perl libxml-libxml-perl libxml-namespacesupport-perl libxml-parser-perl \
-libxml-sax-base-perl libxml-sax-perl libxml-twig-perl linux-libc-dev lxc-pve lxcfs lzop novnc-pve \
-numactl perl-openssl-defaults proxmox-widget-toolkit pve-cluster pve-container pve-docs \
-pve-edk2-firmware pve-firewall pve-ha-manager pve-i18n pve-libspice-server1 pve-qemu-kvm \
-pve-xtermjs python-cephfs python-chardet python-ipaddr python-pkg-resources python-protobuf \
-python-rados python-rbd python-requests python-six python-urllib3 qemu-server smbclient spiceterm \
-sqlite3 thin-provisioning-tools uidmap vncterm xsltproc xz-utils sudo ntfs-3g python3-dbus 
-
-    apt-get -f -y install libjs-extjs6 php-fpm php-json php-cgi php-cli php-mbstring php-pam ethtool python3-dialog acl jfsutils \
-sdparm mdadm libsasl2-modules cpufrequtils uuid nfs-kernel-server proftpd-basic lsb-release sshpass samba \
-samba-common-bin wsdd avahi-daemon libnss-mdns monit acpid beep php-bcmath rrdtool collectd anacron cron-apt \
-quota php-xml quotatool liblocale-po-perl proftpd-mod-vroot libjavascript-minifier-xs-perl xmlstarlet parted \
-nginx pm-utils wpasupplicant samba-vfs-modules python3-pyudev python3-natsort jq ntp python3-netifaces python3-lxml resolvconf
-    rm ./openmediavault_4*.deb
-    wget https://mirrors.tuna.tsinghua.edu.cn/OpenMediaVault/public/pool/main/o/openmediavault/openmediavault_4.1.35-1_all.deb
-    dpkg-deb -x openmediavault_4.1.35-1_all.deb omvtmp 
-    dpkg-deb --control openmediavault_4.1.35-1_all.deb omvtmp/DEBIAN 
-    sed -i "s/watchdog, //g" omvtmp/DEBIAN/control 
-    rm omvtmp/usr/share/openmediavault/engined/module/networking.inc
-    rm omvtmp/usr/share/openmediavault/engined/rpc/network.inc
-    rm omvtmp/usr/share/openmediavault/engined/inc/60networkinterfacebackend.inc
-    dpkg -b omvtmp openmediavault_4.1.35-1_all.deb 
-    dpkg --force-all -i openmediavault_4.1.35-1_all.deb
-    echo "安装完成，下面初始化OpenMediaVault 4!"
-    echo "Installation Complete, now init the OpenMediaVault 4!"
-    rm ./openmediavault_4*.deb
-    rm -rf ./omvtmp
-    omv-initsystem
-    apt-mark hold openmediavault
-
-}
-
-pve54(){
+pve5x(){
     echo "您的系统是：$pve, 您将安装 OpenMediaVault 4"
-    echo -n "Your OS：$pve, you will install OpenMediaVault 4"
     apt-mark unhold openmediavault
     apt-get autoremove openmediavault
     apt-get -f -y install apparmor attr bridge-utils ceph-common ceph-fuse cifs-utils corosync criu cstream dtach ebtables \
@@ -133,16 +72,14 @@ nginx pm-utils wpasupplicant samba-vfs-modules python3-pyudev python3-natsort jq
     dpkg -b omvtmp openmediavault_4.1.35-1_all.deb 
     dpkg --force-all -i openmediavault_4.1.35-1_all.deb
     echo "安装完成，下面初始化 OpenMediaVault 4!"
-    echo "Installation Complete, now init the OMV!"
     rm ./openmediavault_4*.deb
     rm -rf ./omvtmp
     omv-initsystem
     apt-mark hold openmediavault
 }
 
-pve60(){
+pve6x(){
     echo "您的系统是：$pve, 您将安装 OpenMediaVault 5"
-    echo -n "Your OS：$pve, you will install OpenMediaVault 5"
     sleep 2
     cat <<EOF > /etc/apt/sources.list.d/openmediavault.list
 deb https://mirrors.tuna.tsinghua.edu.cn/OpenMediaVault/public usul main
@@ -189,7 +126,6 @@ salt-minion samba samba-vfs-modules sdparm sshpass sudo tdb-tools uuid wpasuppli
     apt-get -y -f install
     dpkg --force-all -i openmediavault_5.5.9-1_all.deb
     echo "安装完成，下面初始化 OpenMediaVault 5!"
-    echo "Installation Complete, now init the OMV!"
     rm -rf ./omvtmp
     omv-confdbadm populate
     apt-mark hold openmediavault
@@ -217,14 +153,11 @@ salt-minion samba samba-vfs-modules sdparm sshpass sudo tdb-tools uuid wpasuppli
 while [ true ] 
     do
         echo "如果您使用的是 zfs 文件系统，请务必给 rpool 做个快照，如果有问题可以回滚快照后重启系统!"
-        echo "If you use zfs rpool for your system, please make sure your rpool has made a snapshot!"
         echo "您的系统是：$pve, 您将安装 OpenMediaVault , 是否继续?(y/n)"
-        echo -n "Your OS：$pve, you will install OpenMediaVault, continue?(y/n)"
         read x
         case "$x" in
         y | yes ) 
-            echo "您的系统是： $pve , 您将安装 OpenMediaVault 4"
-            echo -n "Your OS：$pve, you will install OpenMediaVault 4"
+            echo "您的系统是： $pve , 您将安装 OpenMediaVault "
             echo "deb http://packages.openmediavault.org/public arrakis main" > /etc/apt/sources.list.d/openmediavault.list
             rm openmediavault-keyring_1.0_all.deb
             wget https://mirrors.tuna.tsinghua.edu.cn/OpenMediaVault/public/pool/main/o/openmediavault-keyring/openmediavault-keyring_1.0_all.deb
@@ -232,33 +165,27 @@ while [ true ]
             apt-get update
             rm openmediavault-keyring_1.0_all.deb
             if [ $ver = "5.3" ];then
-                pve53
-                echo "如果没有意外，安装完成! 浏览器打开 http://ip 去试试您的 OpenMediaVault 4!"
-                echo "Installation Complete! Go to http://ip to enjoy OpenMediaVault 4!"
+                pve5x
+                echo "如果没有意外，安装完成! 浏览器打开 http://你的本机IP 去试试您的 OpenMediaVault 4!"
                 exit
             elif [ $ver = "5.4" ];then
-                pve54 
-                echo "如果没有意外，安装完成! 浏览器打开 http://ip 去试试您的 OpenMediaVault 4!"
-                echo "Installation Complete! Go to http://ip to enjoy OpenMediaVault 4!"
+                pve5x
+                echo "如果没有意外，安装完成! 浏览器打开 http://你的本机IP 去试试您的 OpenMediaVault 4!"
                 exit
             elif [ $ver = "6.0" ];then
-                pve60
-                echo "如果没有意外，安装完成! 浏览器打开 http://ip 去试试您的 OpenMediaVault 5!"
-                echo "Installation Complete! Go to http://ip to enjoy OpenMediaVault 5!"
+                pve6x
+                echo "如果没有意外，安装完成! 浏览器打开 http://你的本机IP 去试试您的 OpenMediaVault 5!"
                 exit
             elif [ $ver = "6.1" ];then
-                pve60
-                echo "如果没有意外，安装完成! 浏览器打开 http://ip 去试试您的 OpenMediaVault 5!"
-                echo "Installation Complete! Go to http://ip to enjoy OpenMediaVault 5!"
+                pve6x
+                echo "如果没有意外，安装完成! 浏览器打开 http://你的本机IP 去试试您的 OpenMediaVault 5!"
                 exit
             elif [ $ver = "6.2" ];then
-                pve60
-                echo "如果没有意外，安装完成! 浏览器打开 http://ip 去试试您的 OpenMediaVault 5!"
-                echo "Installation Complete! Go to http://ip to enjoy OpenMediaVault 5!"
+                pve6x
+                echo "如果没有意外，安装完成! 浏览器打开 http://你的本机IP 去试试您的 OpenMediaVault 5!"
                 exit
             else
-                echo "你的 Proxmox VE 版本不是 5.3/5.4/6.0/6.1/6.2 ，不建议安装，即将退出。"
-                echo "Your Proxmox VE version is not 5.3/5.4/6.0/6.1/6.2, it may harm your system, now quit."
+                echo "你的 Proxmox VE 版本不是 5.x/6.x ，不建议安装，即将退出。"
                 break
             fi
         ;;
